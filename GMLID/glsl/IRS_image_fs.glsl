d67e939b8b14407c52c0a62aaa351372ebb26780
DragonMoffon <happydog.aja@gmail.com>
2026-05-12 00:47:22 +1200

    clean-up `IRS_image_fs.glsl` and add blue source origin circle

--- GMLID/glsl/IRS_image_fs.glsl
+++ GMLID/glsl/IRS_image_fs.glsl
@@ -8,11 +8,9 @@ in vec2 vs_uv;
 out vec4 fs_colour;
 
 void main(){
+  vec2 loc = vs_uv * 6.0 - 3.0;
   vec2 pos = texture(texture0, vs_uv).xy;
 
-  fs_colour = texture(texture0, vs_uv)*0.25 + 0.5 + vec4(vec3(1.0 - step(source.z, length(pos - source.xy))), 1.0);
-  /*if (fs_colour.r < 0.0 || 1.0 < fs_colour.r || fs_colour.g < 0.0 || 1.0 < fs_colour.g){
-    fs_colour = vec4(0.0, 0.0, 0.0, 1.0);
-  }*/
-  //fs_colour = texture(texture0, vs_uv);
+  fs_colour = vec4(0.0, 0.0, 1.0 - step(source.z, length(loc - source.xy)), 1.0);
+  fs_colour += vec4(vec3(1.0 - step(source.z, length(pos - source.xy))), 1.0);
 }
